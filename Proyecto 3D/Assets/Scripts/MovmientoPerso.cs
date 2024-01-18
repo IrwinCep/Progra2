393ee3e9fa9b1a356ac9ba7c7d14c66a2b727ba1
IrwinCep <142370646+IrwinCep@users.noreply.github.com>
2024-01-18 11:53:24 -0600

    movimiento

---
+++ Proyecto 3D/Assets/Scripts/MovmientoPerso.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmientoPerso : MonoBehaviour
+{
+    public float veloPerso;
+    private Rigidbody miCuerpo;
+    private Animation miAnimador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        miCuerpo = GetComponent<Rigidbody>();
+        miAnimador = GetComponent<Animation>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movHoriz = Input.GetAxisRaw("Horizontal");
+        float movVert = Input.GetAxisRaw("Vertical");
+
+        miAnimador.SetFloat("Despl_Lat", movHoriz);
+        miAnimador.SetFloat("Despl_Front", movVert);
+
+        miCuerpo.velocity = ((transform.forward *movVert + transform.right *movHoriz) *veloPerso)  ; 
+
+    }
+}
