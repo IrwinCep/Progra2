0767c4eea50c5363218eb855d1a50a424758e16a
IrwinCep <142370646+IrwinCep@users.noreply.github.com>
2024-01-17 23:49:48 -0600

    mov sin animacion

--- Proyecto 3D/Assets/Scripts/CaminandoSimple.cs
+++ Proyecto 3D/Assets/Scripts/CaminandoSimple.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 
 public class CaminandoSimple : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody miCuerpo;
+    public float velPerso;
+    private Animation miAnimador;
+
+    Vector3 movimiento;
     void Start()
     {
-        
+        miCuerpo = GetComponent<Rigidbody>();
+        miAnimador = GetComponent<Animation>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float movHoriz = Input.GetAxisRaw("Vertical");
+        float movVert = Input.GetAxisRaw("Horizontal");
+        movimiento = new Vector3(movHoriz, 0, -movVert);
+    }
+
+    private void FixedUpdate()
+    {
+        miCuerpo.MovePosition(transform.position + (movimiento * velPerso * Time.fixedDeltaTime));
     }
 }
