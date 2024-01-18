95613a8e5e9ce0724010ad2a4e78cf1dc52cbf15
IrwinCep <142370646+IrwinCep@users.noreply.github.com>
2024-01-17 19:21:58 -0600

    movimiento de script

--- Proyecto 3D/Assets/Scripts/CaminandoSimple.cs
+++ Proyecto 3D/Assets/Scripts/CaminandoSimple.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaminadoSimple : MonoBehaviour
+public class CaminandoSimple : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
